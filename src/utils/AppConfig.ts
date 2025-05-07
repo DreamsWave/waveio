8b04f48e8b41dce336c0f6ec32b9363a8c5d55da
DreamsWave <dreamswave.dev@gmail.com>
2025-05-07 18:17:25 +0300

    chore: update supported locales in AppConfig from ['en', 'ru', 'ja', 'lol'] to ['en', 'fr']

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -5,7 +5,7 @@ const localePrefix: LocalePrefixMode = 'as-needed';
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
   name: 'WaveIO',
-  locales: ['en', 'ru', 'ja', 'lol'],
+  locales: ['en', 'fr'],
   defaultLocale: 'en',
   localePrefix,
 };
